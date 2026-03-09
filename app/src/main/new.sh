9b529d9ce87f705ff78722825aa79438b7062a05
va0kean <132040500+va0kean@users.noreply.github.com>
2026-03-09 18:17:10 +0500

    Debug: Disable cache and add stacktrace

--- app/src/main/new.sh
+++ app/src/main/new.sh
@@ -1,4 +1,8 @@
-    git add .
+git add .
+    git commit -m "Setup PWA and GitHub Actions"
+    git push origin maingit add .
+    git commit -m "Debug: Disable cache and add stacktrace"
+    git push origin main    git add .
     git commit -m "Setup PWA and GitHub Actions"
     git push origin main
     
